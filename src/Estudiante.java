f08dd614888e5ddab4483d982a5eb9b7acd78ab8
Mónica <morodriguez112@gmail.com>
2026-07-06 16:09:32 -0500

    Como voy creando el codigo

---
+++ src/Estudiante.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+//Clase Registro estudiantes
+public class Estudiante
+{
+    //informacion estudiante
+    String nombre;
+    int edad;
+    double nota1;
+    double nota2;
+    double nota3;
+}
+//psvm
+void main() {
+    String nombre = JOptionPane.showInputDialog("Ingresar nombre estudiante");
+}
