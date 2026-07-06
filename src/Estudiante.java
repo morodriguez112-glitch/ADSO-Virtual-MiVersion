d3e473bec5ed82c536594d76ea3a85f04fa5b2d1
Mónica <morodriguez112@gmail.com>
2026-07-06 18:00:21 -0500

    Hasta el momento como voy

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -12,5 +12,10 @@ public class Estudiante
 }
 //psvm
 void main() {
-    String nombre = JOptionPane.showInputDialog("Ingresar nombre estudiante");
-}
+    String nombre = JOptionPane.showInputDialog("Ingresar nombre del estudiante") ;
+    int edad = Integer.parseInt(JOptionPane.showInputDialog("Escriba su edad")) ;
+    double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Escribir nota1 del estudiante")) ;
+    double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Escribir nota2 del estudiante")) ;
+    double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Escribir nota3 del estudiante")) ;
+    double promedio = (nota1 + nota2 + nota3) / 3 ;
+    }
