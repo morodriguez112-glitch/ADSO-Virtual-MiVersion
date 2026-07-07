bd638633f4d847e8145e83846f02171ab0974f59
Mónica <morodriguez112@gmail.com>
2026-07-07 11:48:02 -0500

    se realliza correpciones

--- src/Empleado.java
+++ src/Empleado.java
@@ -11,16 +11,16 @@ public class Empleado {
     String nombre = JOptionPane.showInputDialog("Ingresar Nombre del Empleado") ;
     double salario = Double.parseDouble(JOptionPane.showInputDialog("Ingresar salario en número")) ;
     int experiencia_años = Integer.parseInt(JOptionPane.showInputDialog("Ingresar años de experiencia en número")) ;
-    double bono = experiencia_años * 200.000 ;
-    double suma = salario + bono;
-    JOptionPane.showMessageDialog(null, bono + "Bono por años de experiencia ");
-    JOptionPane. showMessageDialog(null, suma + "Salario aplicado con bono para " + nombre);
+    double bono = experiencia_años * 200000 ;
 
     if (experiencia_años > 3) {
-        bono = bono + 300.000 ;
+        bono = bono + 300000 ;
     }
 
     if (experiencia_años > 6) {
-        bono = bono + 400.000 ;
+        bono = bono + 400000 ;
     }
+            double suma = salario + bono;
+            JOptionPane.showMessageDialog(null, bono + "Bono por años de experiencia ");
+            JOptionPane.showMessageDialog(null, suma + "Salario aplicado con bono para " + nombre);
 }
